465f82db55e4d1379e2011ebe4c85020000997b6
André <andrem6169@gmail.com>
2025-09-11 00:34:56 -0300

    refactor: implementados base de métodos da interface BibliotecaService

--- projetoc14/src/main/java/br/c14lab/biblioteca/service/BibliotecaServiceImpl.java
+++ projetoc14/src/main/java/br/c14lab/biblioteca/service/BibliotecaServiceImpl.java
@@ -1,9 +1,56 @@
 package br.c14lab.biblioteca.service;
 
-public class BibliotecaServiceImpl{
+import br.c14lab.biblioteca.model.Emprestimo;
+import br.c14lab.biblioteca.model.Livro;
+import br.c14lab.biblioteca.model.Usuario;
+
+import java.util.List;
+
+public class BibliotecaServiceImpl implements BibliotecaService{
 
 
+    @Override
+    public void cadastrarLivros(Livro livro) {
 
+    }
 
+    @Override
+    public Livro ConsultarLivro(int id) {
+        return null;
+    }
 
+    @Override
+    public List<Livro> listarLivros() {
+        return List.of();
+    }
+
+    @Override
+    public void atualizarLivro(Livro livro) {
+
+    }
+
+    @Override
+    public void removerLivro(int id) {
+
+    }
+
+    @Override
+    public void cadastrarUsuario(Usuario usuario) {
+
+    }
+
+    @Override
+    public void realizarEmprestimo(Emprestimo emprestimo) {
+
+    }
+
+    @Override
+    public void devolverLivro(Livro livro) {
+
+    }
+
+    @Override
+    public List<Emprestimo> listarEmprestimos() {
+        return List.of();
+    }
 }
