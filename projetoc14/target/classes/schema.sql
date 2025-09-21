22fcd1a40c1404b368b5df744e90076d6a1834c8
igoraraujo <igor41660@gmail.com>
2025-09-21 13:16:13 -0300

    Corrigindo o dataBUG

--- projetoc14/target/classes/schema.sql
+++ projetoc14/target/classes/schema.sql
@@ -1,14 +1,3 @@
-
-    //criando as tabelas do banco de dados
-
-CREATE TABLE IF NOT EXISTS usuarios (
-    id VARCHAR(250) PRIMARY KEY,
-    nome VARCHAR(250),
-    email VARCHAR(250),
-    telefone VARCHAR(250),
-    endereco VARCHAR(250)
-);
-
 CREATE TABLE IF NOT EXISTS livros (
     isbn VARCHAR(250) PRIMARY KEY,
     titulo VARCHAR(250),
@@ -17,4 +6,12 @@ CREATE TABLE IF NOT EXISTS livros (
     ano_publicacao INT,
     quantidade_disponivel INT,
     categoria VARCHAR(250)
+);
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id VARCHAR(250) PRIMARY KEY,
+    nome VARCHAR(250),
+    email VARCHAR(250),
+    telefone VARCHAR(250),
+    endereco VARCHAR(250)
 );
